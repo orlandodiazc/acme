43dfe957e13bda2e762bb6d8f9a559ad11a78d42
Orlando Diaz <orlandodiazconde@gmail.com>
2024-11-29 18:23:42 -0500

    refactor rest exception controller

--- backend/src/main/java/com/ditod/acme/exception/EntityAlreadyExistsException.java
+++ backend/src/main/java/com/ditod/acme/exception/EntityAlreadyExistsException.java
@@ -1,14 +1,13 @@
 package com.ditod.acme.exception;
 
 public class EntityAlreadyExistsException extends RuntimeException {
-    private final String field;
+
+    public EntityAlreadyExistsException() {
+        super("Entity does not exist");
+    }
 
     public EntityAlreadyExistsException(String entity, String field) {
         super("A " + entity + " already exists with this " + field);
-        this.field = field;
     }
 
-    public String getField() {
-        return field;
-    }
 }
--- backend/src/main/java/com/ditod/acme/exception/RestExceptionControllerAdvice.java
+++ backend/src/main/java/com/ditod/acme/exception/RestExceptionControllerAdvice.java
@@ -1,28 +1,30 @@
 package com.ditod.acme.exception;
 
-import org.springframework.http.*;
+import com.ditod.acme.exception.EntityDoesNotExistException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.naming.AuthenticationException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 @RestControllerAdvice
-public class RestExceptionControllerAdvice extends ResponseEntityExceptionHandler {
+public class RestExceptionControllerAdvice {
     @ExceptionHandler({EntityDoesNotExistException.class})
-    public ProblemDetail exceptionDoesNotExistHandler(RuntimeException ex) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ProblemDetail handleDoesNotExistException(RuntimeException ex) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status,
-            WebRequest request) {
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         HashMap<String, List<String>> errors = new HashMap<>();
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             String fieldName = error.getField();
@@ -33,18 +35,19 @@ public class RestExceptionControllerAdvice extends ResponseEntityExceptionHandle
         }
         ProblemDetail problemDetail = ex.getBody();
         problemDetail.setProperty("errors", errors);
-        return ResponseEntity.status(problemDetail.getStatus()).body(problemDetail);
-    }
-
-
-    @ExceptionHandler({com.ditod.acme.domain.exception.EntityAlreadyExistsException.class})
-    public ProblemDetail exceptionAlreadyExistsHandler(
-            com.ditod.acme.domain.exception.EntityAlreadyExistsException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        HashMap<String, String> errors = new HashMap<>();
-        errors.put(ex.getField(), ex.getMessage());
-        problemDetail.setProperty("errors", errors);
         return problemDetail;
     }
 
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleAlreadyExistsException(EntityDoesNotExistException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ProblemDetail handleAuthenticationException() {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN,
+                                                "Invalid username or password. Please try again.");
+    }
 }
